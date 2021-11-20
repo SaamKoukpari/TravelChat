b0e200cf4b06f89df696d402f361b60220abeea0
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-20 15:48:05 -0800

    configure routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './Layout';
 
 function App() {
   return (
-    <Layout/>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/login" element={<Login/>} /> */}
+        <Route path="/" element={<Layout/>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
