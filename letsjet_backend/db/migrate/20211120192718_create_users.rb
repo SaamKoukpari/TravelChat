f82b2321f106baf663ec5d215445e1ddf65f7166
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-22 12:05:33 -0800

    seed database

--- letsjet_backend/db/migrate/20211120192718_create_users.rb
+++ letsjet_backend/db/migrate/20211120192718_create_users.rb
@@ -4,17 +4,17 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.integer "friends_id"
       t.integer "post_id"
       t.integer "calendar_id"
+      t.string "name"
       t.string "email"
       t.string "password"
+      t.date "birthday"
       t.string "hometown"
-      t.string "name"
       t.string "location"
       t.string "profile_picture"
       t.text "description"
       t.boolean "active"
       t.boolean "traveller"
       t.boolean "local"
-      t.date "birthday"
     end
   end
 end
--- letsjet_backend/db/seeds.rb
+++ letsjet_backend/db/seeds.rb
@@ -8,20 +8,44 @@
 
 User.destroy_all
 
-lucy = User.create(name: 'Lucy', password: 'password')
-
-# lucy.avatar.attach(
-#   io: File.open('./public/avatars/goTeam.png'),
-#   filename: 'goTeam.png',
-#   content_type: 'application/png'
-# )
-
 User.create!(
-  name: 'Amazon',
-  email: 'StaffSoftware@Engineer',
+  name: 'Lucy', 
+  email: 'lucy@littlemermaid.com',
+  password: 'password',
+  birthday: '1994-04-07',
+  hometown: 'Vancouver',
+  location: 'Barcelona',
+  profile_picture: 'https://video-images.vice.com/_uncategorized/1630621230611-gettyimages-115363484.jpeg',
+  description: 'Little mermaid from the Pacific looking to sight-see and cliff-dive in the Mediterranean',
+  active: true,
+  traveller: true,
+  local: false
 )
 
-# User.create!(
-#   name: 'Microsoft',
-#   email: 'Engineer@Engineer',
-# )
+User.create!(
+  name: 'Shaun', 
+  email: 'shaun@robs.ca',
+  password: 'password2',
+  birthday: '1989-06-15',
+  hometown: 'Vancouver',
+  location: 'Vancouver',
+  profile_picture: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ct-white-4560-1619794724.jpg',
+  description: 'West Coast born and raised, male model looking for friends who want to do photoshoots in the mountains!',
+  active: true,
+  traveller: false,
+  local: true
+)
+
+User.create!(
+  name: 'Simba', 
+  email: 'lions@google.com',
+  password: 'password3',
+  birthday: '1993-04-07',
+  hometown: 'Toronto',
+  location: 'Bali',
+  profile_picture: 'https://images.goodsmile.info/cgm/images/product/20200114/9164/66743/large/04c45b878e5a2be879d9137a18905611.jpg',
+  description: 'Lion from the Jungle looking to travel and ATV in the Saharan Desert',
+  active: true,
+  traveller: true,
+  local: false
+)
