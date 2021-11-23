e4907286a922f43cced51bcf0d1b5b9e4059fad8
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-22 16:11:29 -0800

    fix profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -10,22 +10,13 @@ export default function Profile () {
   useEffect(() => {
     axios.get('/api/users')
     .then(response => {
-<<<<<<< HEAD
-      //const name = user[0].data;
-      setUser(response.data[1])
-=======
       // const name = response.data;
       setUser(response.data[0])
->>>>>>> f82b2321f106baf663ec5d215445e1ddf65f7166
     })
     .catch(error => console.log("error:", error))
     }, [])
     
-<<<<<<< HEAD
-    console.log("User array:", user)
-=======
     // console.log(user)
->>>>>>> f82b2321f106baf663ec5d215445e1ddf65f7166
 
   return (
     <div>
