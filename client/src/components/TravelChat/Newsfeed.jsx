7d95c90bb7d7fd6d86a294bd4238bfd20ce0d2f0
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-29 19:17:22 -0800

    fix newsfeed

--- client/src/components/TravelChat/Newsfeed.jsx
+++ client/src/components/TravelChat/Newsfeed.jsx
@@ -6,8 +6,9 @@ import { Avatar } from "@mui/material";
 
 export default function Newsfeed() {
   const [state, setState] = useState([]);
-  const [newPost, setNewPost] = useState([]);
-  const userID = 1;
+  const [newPost, setNewPost] = useState("");
+  const [users, setUsers] = useState([]);
+  const [userID, setUserID] = useState(1);
 
   const func = function () {
     Promise.all([
@@ -19,66 +20,61 @@ export default function Newsfeed() {
         const users = all[0].data; //all the users
         const posts = all[1].data; //all the posts
         const comments = all[2].data; //all the comments
+        setUsers(users);
 
-        //filter user.id = 1 (Lucy)
-        const user1 = users.find((user) => {
-          return user.id === userID;
-        });
-        //filter user1 friends
-        const usersFriends = user1.friend_id.map((id) => {
-          const friends = users.find((user) => user.id === id);
-          return friends;
-        });
-        // if friend has a post_id, then return the posts
-        const postsByFriends = function (usersFriends) {
-          //array of post.ids
-          const result = [];
-          for (let obj of usersFriends) {
-            if (obj.post_id.length >= 1) {
-              result.push(obj.post_id);
-            }
-          }
-          return result.flat(Infinity); //remove inner Arrays
+        const getParsedPosts = () => {
+          //populating post object
+          return posts
+            .map((post) => {
+              let user = users.find((u) => u.id === post.user_id);
+
+              return {
+                user,
+                ...post,
+              };
+            })
+            .sort((a, b) => {
+              console.log("ONE:", a);
+              console.log("TWO:", new Date(b.created_at));
+              return (
+                new Date(b.created_at).getTime() -
+                new Date(a.created_at).getTime()
+              );
+            });
         };
-        //match the postsByFriends item to the post.id, return content, likes, comments, posted_at
-        const getPosts = postsByFriends(usersFriends).map((id) => {
-          const post = posts.find((x) => x.id === id);
-          const findUser = Object.values(users).find((user) =>
-            user.post_id.includes(id)
-          );
-          return [findUser, post.content, post.likes, post.posted_at];
-        });
-        setState(getPosts);
 
-        //if the post has comments, return the comments
+        setState(getParsedPosts());
       })
 
       .catch((err) => err);
   };
 
   useEffect(() => {
-    func() 
+    func();
   }, []);
 
-  const newsfeedPosts = state.map((post) => {
-    //state is an ARRAY not an object
-    return (
-      <PostItem
-        key={post.id}
-        user={post[0]} //this is an object
-        content={post[1]}
-        likes={post[2]}
-        time={post[3]}
-      />
-    );
-  });
-  console.log("STATE>>>", state)
+  const newsfeedPosts = state
+    .map((post) => {
+      const user = users.find((u) => u.id === userID);
+      //state is an ARRAY not an object
+      if (user.friend_id.includes(post.user_id) || userID === post.user_id) {
+        return (
+          <PostItem
+            key={post.id}
+            post={post}
+          />
+        );
+      }
+    })
+    .filter(Boolean);
+
+  console.log("STATE>>>", state);
 
   const createPost = (e) => {
     e.preventDefault();
 
     const posted = {
-      user_id: 3,
+      user_id: 1,
       content: newPost,
     };
     console.log("POST: ", posted);
@@ -86,41 +82,8 @@ export default function Newsfeed() {
     axios
       .post("/api/posts", posted)
       .then((res) => {
-        const currentPost = res.data;
-        console.log("currentPost", currentPost);
-
-        const update = {
-          post_id: currentPost.id,
-          //.array.isRequired
-        };
-
-        // const updateArray = Object.keys(update)
-
-        axios.put("/api/users/3", update).then((res) => {
-          console.log("PUT RES", res.data);
-          const user3Obj = res.data;
-          console.log("USEROBJ", user3Obj.post_id);
-          const postArray = user3Obj.post_id;
-
-          // for (post_id in user3Obj) {
-          //   console.log("???", user3Obj.post_id)
-          //   return user3Obj.post_id
-          // }
-
-          // Object.keys(user3Obj).forEach(function(key) {
-
-          //   return user3Obj.post_id
-          // })
-
-          // const posts = postArray.map((id) => {
-
-          //    const post = res.data.find((post) => post.id === id);
-          //    return post;
-          // });
-          // console.log("LOOK: ", posts)]
-          func();
-          // setNewPost(postArray)
-        });
+        func();
+        setNewPost("");
       })
       .catch((err) => err);
   };
@@ -128,7 +91,12 @@ export default function Newsfeed() {
   return (
     <div className="main_newsfeed_container">
       <section className="create_post">
-        <Avatar className="user-post" alt="user_loggedin" src="./lucy-angel.png" sx={{ width: 46, height: 46 }} />
+        <Avatar
+          className="user-post"
+          alt="user_loggedin"
+          src="./lucy-angel.png"
+          sx={{ width: 46, height: 46 }}
+        />
         <form>
           <input
             className="create_post_field"
--- client/src/components/TravelChat/PostItem.jsx
+++ client/src/components/TravelChat/PostItem.jsx
@@ -5,8 +5,8 @@ import TimeAgo from 'timeago-react';
 import Icon from '@mui/material/Icon';
 import { Avatar } from "@mui/material";
 
-export default function PostItem(props){
-  const [countLike, setCountLike] = useState(props.likes)
+export default function PostItem({post={}}){
+  const [countLike, setCountLike] = useState(post.likes)
   const [color, setColor] = useState(false);
   
   // Functions to change  the counter state item
@@ -15,26 +15,27 @@ export default function PostItem(props){
     setCountLike(countLike + 1);
   };
   
+
   return(
     <div>
       <section className="news_item">
         <div className="news-item-header">
           <table>
             <tr>
-            <td><Avatar className="user-post" alt="user_loggedin" src={props.user.profile_picture} sx={{ width: 46, height: 46 }}/></td>
-            <td>{props.user.name}</td>
+            <td><Avatar className="user-post" alt="user_loggedin" src={post.user.profile_picture} sx={{ width: 46, height: 46 }}/></td>
+            <td>{post.user.name}</td>
             </tr>
           </table>
         </div>
         <div className="news-item-body">
-        <span>{props.content}</span>
+        <span>{post.content}</span>
         </div>
         <div className="news-item-footer">
           <div className="heart-count">
               <Icon className={color === true ? 'active-like' : ''} onClick={(e) => increment(e.target.value)}>favorite</Icon>
               {countLike}
           </div>
-        <TimeAgo datetime={props.time}/>
+        <TimeAgo datetime={post.created_at}/>
         </div>
 
       </section>
